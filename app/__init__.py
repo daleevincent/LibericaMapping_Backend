a856d660334d4dac1fe7b2e6e505307f5b0f882b
itzjmbruhhh <reyesjohnmichael1029@gmail.com>
2026-03-09 23:54:25 +0800

    fixed mongo initialization on __init__.py and added lazy collection farm_model.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,13 @@ from flask_cors import CORS
 from .database.mongo import init_mongo
 from .routes.prediction_routes import prediction_bp
 from .routes.farm_routes import farm_bp
+from app.database.mongo import init_mongo
 
 def create_app():
 
     app = Flask(__name__)
     CORS(app)
-
-    init_mongo(app)
+    init_mongo(app)  # must be before routes are used
 
     app.register_blueprint(prediction_bp)
     app.register_blueprint(farm_bp)
--- app/models/farm_model.py
+++ app/models/farm_model.py
@@ -1,17 +1,20 @@
-from app.database.mongo import db
+from app.database.mongo import db as get_db
+from app.database import mongo as mongo_module
 
 class FarmModel:
 
-    collection = db["farms"]
+    @staticmethod
+    def get_collection():
+        return mongo_module.db["farms"]
 
     @staticmethod
     def create(data):
-        return FarmModel.collection.insert_one(data)
+        return FarmModel.get_collection().insert_one(data)
 
     @staticmethod
     def get_all():
-        return list(FarmModel.collection.find())
+        return list(FarmModel.get_collection().find())
 
     @staticmethod
     def get_by_owner(owner_id):
-        return list(FarmModel.collection.find({"owner_id": owner_id}))
+        return list(FarmModel.get_collection().find({"owner_id": owner_id}))
